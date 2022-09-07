8cf2bd224d0bcd836f0f74a5feedf931acb744b8
zhaojianguo <zhaojianguo@zhidaoauto.com>
2022-09-07 16:03:34 +0800

    feat 给机柜添加信息面板

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import MachineRoom from './component/MachineRoom';
 
@@ -14,6 +14,25 @@ let canvas: HTMLCanvasElement;
  
 //
 function App() {
+  //信息面板的位置
+  const [planePos, setPlanePos] = useState({
+    left: 0,
+    top:0
+  })
+  //信息面板的可见性
+  const [planeDisplay, setPlaneDisplay] = useState('none')
+  //机柜信息
+  const [curCabinet,setCurCabinet] = useState({
+    //名称
+    name:'Loading……',
+    //温度
+    temperature: 0,
+    //容量
+    capacity: 0,
+    //服务器数量
+    count:0
+  })
+  
 
   // 组件挂在完毕后，实例化机房对象，渲染机房
   useEffect(()=>{
@@ -27,18 +46,44 @@ function App() {
     room = new MachineRoom(canvas);
     room.loadGLTF('machineRoom.gltf');
     room.animate();
-  },[])
 
+    
+
+    // 当鼠标移入机会，显示信息面板
+    room.onMouseOverCabinet  = ()=>{
+      setPlaneDisplay('block')
+    }
+
+    // 当鼠标在机柜上移动，就让信息面板跟随移动
+    room.onMouseMoveCabinet = (left,top)=>{
+      setPlanePos({
+        left,top
+      })
+    }
+
+    // 当鼠标划出机柜，隐藏信息面板
+    room.onMouseOutCabinet = ()=>{
+      setPlaneDisplay('none')
+    }
+  },[])
 
   // 鼠标移动事件
   const mouseMove = ({clientX,clientY})=>{
     room.selectCabinet(clientX,clientY)
   }
 
+
+  
+
   // 建立canvas画布后，并通过ref获取其HTMLCanvasElement对象 
   return (
     <div className="App" onMouseMove={mouseMove}>
       <canvas id='canvas' ref={(ele) => (canvas = ele)}></canvas>
+      <div id='plane' style={{left:planePos.left,top:planePos.top,display:planeDisplay}} >
+        <p>机柜名称：{curCabinet.name}</p>
+        <p>机柜温度：{curCabinet.temperature}°</p>
+        <p>使用情况：{curCabinet.count}/{curCabinet.capacity}</p>
+      </div>
     </div>
   );
 }
