34ff3adb6db0f1568f8072d63c1e87833ba6ded2
zhaojianguo <zhaojianguo@zhidaoauto.com>
2022-09-08 22:20:19 +0800

    feat 给材质添加光源

--- src/views/RenderStructure.tsx/index.tsx
+++ src/views/RenderStructure.tsx/index.tsx
@@ -21,9 +21,21 @@ const renderer = new WebGLRenderer();
 renderer.setSize(innerWidth, innerHeight);
 
 const geometry = new BoxGeometry();
-const material = new MeshNormalMaterial();
+// const material = new MeshNormalMaterial(); // 这个材质不支持光源
+const material = new MeshPhongMaterial({ color: 0x44aa88 }); // 这个材质是支持光源的
 const cube = new Mesh(geometry, material);
 scene.add(cube);
+
+// 先定义平行光的颜色
+const color = 0xffffff; 
+// 定义光照强度
+const intensity = 1;
+// 实例话平行光
+const light = new DirectionalLight(color, intensity);
+// 设置下光源的位置
+light.position.set(-1,2,4);
+// 添加平行光源
+scene.add(light);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -46,7 +58,7 @@ const RenderStructure: React.FC = ():JSX.Element =>{
     }
     
   }, []);
-  return <div ref={divRef}></div>;
+  return <div ref={divRef}></div>; 
 
 }
 
